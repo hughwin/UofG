7e1d4be5706e5b57e62678e903f7fd12e738a009
hughwin <hugh.winchester@gmail.com>
2020-04-19 16:41:30 +0100

    Finished file IO

---
+++ Programming/ExamPrep/src/FileIO.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileIO {
+
+    public static void loadSumFile(){
+
+        File sum = new File("Programming/ExamPrep/sum.txt");
+        try {
+            Scanner scanner = new Scanner(sum);
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                String[] stringArray = line.split(" ");
+                if(stringArray[1].equals("+")) {
+                    System.out.println(Integer.parseInt(stringArray[0]) + Integer.parseInt(stringArray[2]));
+                }
+                else{System.out.println(Integer.parseInt(stringArray[0]) - Integer.parseInt(stringArray[2]));
+                }
+            }
+        }catch (FileNotFoundException fileNotFoundException){
+            fileNotFoundException.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        loadSumFile();
+    }
+
+}
